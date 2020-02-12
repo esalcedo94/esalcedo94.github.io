9d03df1e54d5b5ce9dd7c9eabeff4ef0ed82bc2b
salcedoe <esalcedo94@yahoo.com>
2020-02-11 16:34:45 -0800

    Getting meals on page and having button for instructions done.

--- meal-app/app.js
+++ meal-app/app.js
@@ -1,19 +1,27 @@
 
-
 $(() => {
-  $.ajax({
-    url: "https://www.themealdb.com/api/json/v1/1/random.php"
-  }).then(
-    (data) => {
-      const pic = 
-    },
-    () => {
-      console.log("fatal error");
-    }
-  )
+  const $picHolder = $(".meal-preview");
+  for (let i = 1; i <= 4; i++) {
+    $.ajax({
+      url: "https://www.themealdb.com/api/json/v1/1/random.php"
+    }).then(
+      (data) => {
+        let picURL = data.meals[0].strMealThumb;
+        let imgId =data.meals[0].strMeal;
+        // console.log(picURL);
+        $("#img" + i).attr('src' , picURL);
+        $("#img" + i).attr('id' , imgId);
+      },
+      () => {
+        console.log("fatal error");
+      }
+    )
+  }
+  
   
   $('input[type="submit"]').on('click', () => {
-    // $('.complaints').empty();
+    $picHolder.empty();
+    
     // let userInput = event.target.value.toUpperCase();
     const userInput = $('input[type="text"]').val();
     event.preventDefault();
@@ -21,7 +29,28 @@ $(() => {
       url: "https://www.themealdb.com/api/json/v1/1/search.php?s=" + userInput
     }).then(
       (data) => {
-        console.log(data);
+        // let ingredients = [];
+        // data.meals.forEach((element) => {
+        //   for (let i = 0; i < 20; i++) {
+        //     ingredients.push([element["strIngredient" + (i + 1)] , element["strMeasure" + (i + 1)]]);
+        //     // console.log(element["strIngredient" + (i + 1)]);
+        //     // console.log(element["strIngredient" + (i + 1)]);
+        //   }
+        // })
+        for (let i = 0; i < data.meals.length; i++) {
+          console.log(data.meals[i]);
+          const mealName = data.meals[i].strMeal;
+          console.log(mealName);
+          const $mealTitle = $("<h3>").html(mealName);
+          $mealTitle.appendTo($picHolder);
+          const $instructionsButton = $("<button>").text("Instructions");
+          $instructionsButton.appendTo($mealTitle);
+          const mealInstructions = data.meals[i].strInstructions;
+          const $instructions = $("<p>").html(mealInstructions).appendTo($mealTitle).hide();
+          $instructionsButton.on("click", () => {
+            $instructions.toggle();
+          })
+        }
       },
       () => {
         console.log("fatal error");
